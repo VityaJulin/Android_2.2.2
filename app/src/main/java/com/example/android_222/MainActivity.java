ab26d0cb00102c3fe17bda6ffe58758438b7a2aa
vityajulin <vkdvornokov@gmail.com>
2020-04-13 19:40:18 +0300

    add address and money to git

--- app/src/main/java/com/example/android_222/MainActivity.java
+++ app/src/main/java/com/example/android_222/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.android_222;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
